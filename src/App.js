b709370f86be0702df7d03b7c89f2bc34c694b53
Bradley_How <bradley-henry.how-chen-nian.1@ens.etsmtl.ca>
2019-04-06 19:28:30 -0400

    start mp3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 /*global google*/
 
 import _ from 'lodash'
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import renderIf from "render-if";
 import styled, { css } from "styled-components";
 import firebase from "firebase";
@@ -16,9 +16,10 @@ import {
   OverlayView
 } from "react-google-maps";
 import { Column, Table } from "react-virtualized";
-
+import ReactPlayer from "react-player";
 import Template from "./components/template";
 import NavBar from "./components/NavBar/NavBar";
+import song from "./assets/song1.mp3";
 
 
 // google account: oglavichackathons@gmail.com
@@ -48,10 +49,79 @@ const RowDiv = styled.div`
   align-items: center;
 `;
 
+const MusicDiv = styled.div`
+  position:absolute;
+  display:flex;
+  width:100%;
+  height:10%;
+  background-color:#000;
+  top:90%;
+`;
+const SuperDiv = styled.div`
+  /* height: 1000px; */
+  position:absolute;
+  top: 10%;
+  display:flex;
+  color:#000;
+  font-size:14px;
+`;
+const PlayButton = styled.button`
+  height:50px;
+  width:50px;
+`;
+
 export default function App() {
+  
+  const player = useRef(null); 
+  const [playing, setPlaying] = useState(true);
+  const [loop,setLoop] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const playPauseMusic = () =>{
+    setPlaying(!playing);
+  };
+  // const onPlay = () => {
+  //   console.log('onPlay');
+  //    setPlaying(true);
+  // }
+  // const onPause = () =>{
+  //   console.log('Onpause');
+  //   setPlaying(false);
+  // }
+  // const onEnded = () =>{
+  //   setLoop(false);
+  // }
+  const onDuration = (duration) =>{
+
+    console.log('onDuration', duration)
+    setDuration(duration);
+  };
+  // const onProgress = state => {
+  //     console.log('onProgress', state)
+  //     // if (!this.state.seeking) {
+  //     //   this.setState(state)
+  //     // }
+  //   }
+  useEffect(()=>{
+    console.log(player)
+  })
   return (
     <>
-      <NavBar />
+      <NavBar>
+      
+      
+      </NavBar>
+        {/* <SuperDiv>{ (( player.current && player.current.getCurrentTime()===null)? "1": player.current.getCurrentTime())}</SuperDiv> */}
+      <MusicDiv>
+        <PlayButton onClick={playPauseMusic}/>
+      <ReactPlayer
+              ref={player}
+              width='0%'
+              height='0%'
+              url={song}
+              playing={playing}
+              duration={onDuration}
+            />
+      </MusicDiv>
     </>
   );
 }
